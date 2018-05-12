aa5eefe2400734cf037e2d332d50b6f6ade69fff
Victor Moura <victor_cmoura@hotmail.com>
2018-05-12 02:26:25 -0300

    Added latest builds endpoints

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -107,6 +107,141 @@ class BuildsController < ApplicationController
     render json: @all_builds
   end
 
+  # GET /builds/latest
+  def show_latest
+    @result = []
+
+    @last_build = Build.all.last
+    if @last_build
+      @latest_builds = Build.where(:master_tag => @last_build.master_tag, :dev_tag => @last_build.dev_tag)
+      @latest_builds.each do |build|
+        @result.append({
+          build: {
+            id: build.id,
+            version: build.master_tag.to_s + '.' + build.dev_tag.to_s,
+            plataform: build.plataform
+          },
+          file: {
+            id: build.file_object.id,
+            file_name: build.file_object.file_name,
+            file_size: build.file_object.file_size,
+            url: '/file_objects/' + build.file_object.id.to_s + '/' + build.file_object.file_name
+          }
+        })
+      end
+    end
+
+    render json: @result
+  end
+
+  # GET /builds/latest_stable/
+  def show_latest_stable
+    @result = []
+
+    @last_build = Build.all.last
+    if @last_build
+      @latest_builds = Build.where(:master_tag => @last_build.master_tag, :dev_tag => 0)
+      @latest_builds.each do |build|
+        @result.append({
+          build: {
+            id: build.id,
+            version: build.master_tag.to_s + '.' + build.dev_tag.to_s,
+            plataform: build.plataform
+          },
+          file: {
+            id: build.file_object.id,
+            file_name: build.file_object.file_name,
+            file_size: build.file_object.file_size,
+            url: '/file_objects/' + build.file_object.id.to_s + '/' + build.file_object.file_name
+          }
+        })
+      end
+    end
+
+    render json: @result
+  end
+
+  # GET /builds/latest_stable/Windows
+  def show_latest_stable_windows
+    @result = []
+
+    @last_build = Build.all.last
+    if @last_build
+      @latest_builds = Build.where(:master_tag => @last_build.master_tag, :dev_tag => 0, :plataform => 'Windows')
+      @latest_builds.each do |build|
+        @result.append({
+          build: {
+            id: build.id,
+            version: build.master_tag.to_s + '.' + build.dev_tag.to_s,
+            plataform: build.plataform
+          },
+          file: {
+            id: build.file_object.id,
+            file_name: build.file_object.file_name,
+            file_size: build.file_object.file_size,
+            url: '/file_objects/' + build.file_object.id.to_s + '/' + build.file_object.file_name
+          }
+        })
+      end
+    end
+
+    render json: @result
+  end
+
+  # GET /builds/latest_stable/Linux
+  def show_latest_stable_linux
+    @result = []
+
+    @last_build = Build.all.last
+    if @last_build
+      @latest_builds = Build.where(:master_tag => @last_build.master_tag, :dev_tag => 0, :plataform => 'Linux')
+      @latest_builds.each do |build|
+        @result.append({
+          build: {
+            id: build.id,
+            version: build.master_tag.to_s + '.' + build.dev_tag.to_s,
+            plataform: build.plataform
+          },
+          file: {
+            id: build.file_object.id,
+            file_name: build.file_object.file_name,
+            file_size: build.file_object.file_size,
+            url: '/file_objects/' + build.file_object.id.to_s + '/' + build.file_object.file_name
+          }
+        })
+      end
+    end
+
+    render json: @result
+  end
+
+  # GET /builds/latest_stable/OSX
+  def show_latest_stable_osx
+    @result = []
+
+    @last_build = Build.all.last
+    if @last_build
+      @latest_builds = Build.where(:master_tag => @last_build.master_tag, :dev_tag => 0, :plataform => 'OSX')
+      @latest_builds.each do |build|
+        @result.append({
+          build: {
+            id: build.id,
+            version: build.master_tag.to_s + '.' + build.dev_tag.to_s,
+            plataform: build.plataform
+          },
+          file: {
+            id: build.file_object.id,
+            file_name: build.file_object.file_name,
+            file_size: build.file_object.file_size,
+            url: '/file_objects/' + build.file_object.id.to_s + '/' + build.file_object.file_name
+          }
+        })
+      end
+    end
+
+    render json: @result
+  end
+
   # GET /builds/1
   def show
     render json: @build
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/builds/latest', to: 'builds#show_latest'
+  get '/builds/latest_stable', to: 'builds#show_latest_stable'
+  get '/builds/latest_stable/Windows', to: 'builds#show_latest_stable_windows'
+  get '/builds/latest_stable/Linux', to: 'builds#show_latest_stable_linux'
+  get '/builds/latest_stable/OSX', to: 'builds#show_latest_stable_osx'
   get '/file_objects/:id/:filename', to: 'file_objects#download'
   get '/builds/Windows', to: 'builds#windows_builds'
   get '/builds/Linux', to: 'builds#linux_builds'

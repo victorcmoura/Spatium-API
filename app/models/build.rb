f9c760f1ad146e5c3f492c9c18e403af5dfa5f18
Victor Moura <victor_cmoura@hotmail.com>
2018-05-25 11:44:46 -0300

    Inserting dev builds quantity limit

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,3 +1,20 @@
 class Build < ApplicationRecord
+  before_save :limit_amazon_bucket
   has_one :file_object
+
+  private
+
+  # Method to avoid Carrot's bankrupcy
+  def limit_amazon_bucket
+    if !self.is_master
+      while Build.where(:is_master => [nil, false]).count > 30
+        @exclude_build = Build.where(:is_master => [nil, false]).first
+        @file = @exclude_build.file_object
+
+        @file.destroy
+        @exclude_build.destroy
+      end
+    end
+  end
+
 end
